bdd095c48470ca17f9232eec49b9e37244758581
LegoHawk <73064598+LegoHawk@users.noreply.github.com>
2021-11-24 13:10:06 +0000

    Update archive_with_request_import.py
    Deleted hello line - redundant

--- archive_with_request_import.py
+++ archive_with_request_import.py
@@ -1,7 +1,6 @@
 import webbrowser
 import requests
 
-print("Hello Sir/Madam! (or however you identify)")
 print("Let's find an old website.")
 site = input("Please type a website URL: ")
 era = input("Type a year, month, and day, like 20150613: ")
