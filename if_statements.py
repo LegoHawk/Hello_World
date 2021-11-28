92f30dfe9beac810a58a457ba44d85e241d8e570
LegoHawk <73064598+LegoHawk@users.noreply.github.com>
2021-11-28 20:17:19 +0000

    Added if statements

---
+++ if_statements.py
@@ -0,0 +1,15 @@
+furry = 0
+large = 0
+
+if furry:
+    if large:
+        print("It's a yetti")
+    else:
+        print("It's a cat")
+else:
+    if large:
+        print("It's a whale!")
+    else:
+        print("It's a human. Or a hairless cat")
+
+color = "mauve"
